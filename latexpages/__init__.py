f7cc0b538c9bbb098e5ad36ed762c0609a50f240
Sebastian Bank <sebastian.bank@uni-leipzig.de>
2016-09-06 21:33:50 +0200

    move package metadata below imports

--- latexpages/__init__.py
+++ latexpages/__init__.py
@@ -2,14 +2,14 @@
 
 """Combine a collection of LaTeX documents into a single PDF file."""
 
-__title__ = 'latexpages'
-__version__ = '0.6.dev0'
-__author__ = 'Sebastian Bank <sebastian.bank@uni-leipzig.de>'
-__license__ = 'MIT, see LICENSE'
-__copyright__ = 'Copyright (c) 2014-2016 Sebastian Bank'
-
 from .building import make
 from .numbering import paginate
 from .cleaning import clean
 
 __all__ = ['make', 'paginate', 'clean']
+
+__title__ = 'latexpages'
+__version__ = '0.6.dev0'
+__author__ = 'Sebastian Bank <sebastian.bank@uni-leipzig.de>'
+__license__ = 'MIT, see LICENSE'
+__copyright__ = 'Copyright (c) 2014-2016 Sebastian Bank'
