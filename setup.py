4d42cfe73e77a3502eb397550f27a0a898001a6b
Sebastian Bank <sebastian.bank@uni-leipzig.de>
2017-03-25 07:21:56 +0100

    bump version for development

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='latexpages',
-    version='0.6.1',
+    version='0.6.2.dev0',
     author='Sebastian Bank',
     author_email='sebastian.bank@uni-leipzig.de',
     description='Combine LaTeX docs into a single PDF',
