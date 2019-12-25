9b7a7c4ff8c3f8de343e9b3291d319138770184f
Sebastian Bank <sebastian.bank@uni-leipzig.de>
2019-12-25 13:03:57 +0100

    release 0.6.7

--- latexpages/__init__.py
+++ latexpages/__init__.py
@@ -9,7 +9,7 @@ from .cleaning import clean
 __all__ = ['make', 'paginate', 'clean']
 
 __title__ = 'latexpages'
-__version__ = '0.6.7.dev0'
+__version__ = '0.6.7'
 __author__ = 'Sebastian Bank <sebastian.bank@uni-leipzig.de>'
 __license__ = 'MIT, see LICENSE.txt'
-__copyright__ = 'Copyright (c) 2014-2018 Sebastian Bank'
+__copyright__ = 'Copyright (c) 2014-2019 Sebastian Bank'
