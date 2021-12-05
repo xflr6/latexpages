dc192e985ed258b1408bce669fb9ed50f4af533c
Sebastian Bank <sebastian.bank@uni-leipzig.de>
2021-12-05 15:20:25 +0100

    bump version for development

--- latexpages/__init__.py
+++ latexpages/__init__.py
@@ -7,7 +7,7 @@ from .numbering import paginate
 __all__ = ['make', 'paginate', 'clean']
 
 __title__ = 'latexpages'
-__version__ = '0.7'
+__version__ = '0.7.1.dev0'
 __author__ = 'Sebastian Bank <sebastian.bank@uni-leipzig.de>'
 __license__ = 'MIT, see LICENSE.txt'
 __copyright__ = 'Copyright (c) 2014-2021 Sebastian Bank'
