0cbd2a7f33f95ef1530ed7405038ecf83c115f91
Sebastian Bank <sebastian.bank@uni-leipzig.de>
2022-01-01 15:57:00 +0100

    drop Python 3.6 support

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     package_data={'latexpages': ['template.tex', 'settings.ini']},
     zip_safe=False,
     platforms='any',
-    python_requires='>=3.6',
+    python_requires='>=3.7',
     extras_require={
         'dev': ['flake8', 'pep8-naming', 'wheel', 'twine'],
         'test': ['coverage'],
@@ -37,7 +37,6 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
